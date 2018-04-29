7b16effcd334a3ac2ee7c7ce8ee4b8bd5748bca2
Varuna Eeriyaulla <lex.varuna@gmail.com>
2018-04-29 19:38:10 +0530

    Insert and remove Players

--- client/main.js
+++ client/main.js
@@ -1,1 +1,49 @@
-console.log('File from client');
+import React from "react";
+import ReactDOM from "react-dom";
+import {Meteor} from "meteor/meteor";
+import { Tracker } from "meteor/tracker";
+import Players from './../imports/api/players';
+
+let renderPlayers = (playerList) => {
+    return playerList.map((player) => {
+        return (
+        <div key={player._id}>
+            <p>
+                Player Name : {player.name} has scored : {player.score} 
+                <button onClick={() => Players.remove({_id: player._id})}> &times; </button>
+            </p>
+        </div>);
+    })
+};
+
+const handleSubmit = (e) => {
+    let playerName = e.target.playerName.value;
+    let playerScore = e.target.playerScore.value;
+    e.preventDefault();
+    if (playerName) e.target.playerName.value = '';
+    if (playerScore) e.target.playerScore.value = '';
+
+    Players.insert({
+        name: playerName,
+        score: playerScore
+    });
+};
+
+Meteor.startup(function () {
+    Tracker.autorun(() => {
+        let players = Players.find().fetch();
+        let jsx = (
+            <div>
+                <p>Hello</p>
+                Dinamic Render : {renderPlayers(players)}
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="playerName" placeholder="Player Name" />
+                    <input type="text" name="playerScore" placeholder="Player Score" />
+                    <input type="submit" value="Add" />
+                </form>
+            </div>
+        );
+        ReactDOM.render(jsx, document.getElementById('app'));
+    
+    });
+});
--- server/main.js
+++ server/main.js
@@ -1,1 +1,14 @@
-console.log('File from server');
+import { Meteor } from "meteor/meteor";
+import Players from './../imports/api/players';
+
+Meteor.startup(function () {
+    // Players.insert(
+    //     {
+    //         name: "Panasara",
+    //         score: 11
+    //     }
+    // );
+    // console.log(Players.find().fetch());
+    console.log(Players.find().fetch());
+    
+});
